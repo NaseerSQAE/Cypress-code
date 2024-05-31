24b9dc60e84385857f053ee89d915e9ffd75e985
naseer <ahmadnaseer8344@gmail.com>
2024-05-31 11:15:16 +0500

    new changes

--- cypress/e2e/s.cy.js
+++ cypress/e2e/s.cy.js
@@ -5,7 +5,7 @@ describe('Sign-In Test', () => {
       cy.visit('http://34.242.225.194:5000/signin/ao');
       cy.get('#email').type('asifbangash332@gmail.com');
       cy.get('#password').type('Test@123');
-      
+    //   Hell world
       cy.get('.px-1').click();
       cy.get('.block.lg\\:hidden.text-xl').click({ multiple: true })
 });
